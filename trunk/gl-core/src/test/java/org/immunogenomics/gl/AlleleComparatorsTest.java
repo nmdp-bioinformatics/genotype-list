0b25b8f3018035226af5db58aed5e2478f810672
mheuer@nmdp.org <mheuer@nmdp.org@a202cc79-c986-5676-df02-e9c59fcde34c>
2012-07-26 16:56:05 +0000

    Issue 16 ; adding support for IMGT allele nomenclature G groups

--- trunk/gl-core/src/test/java/org/immunogenomics/gl/AlleleComparatorsTest.java
+++ trunk/gl-core/src/test/java/org/immunogenomics/gl/AlleleComparatorsTest.java
@@ -47,7 +47,7 @@ public final class AlleleComparatorsTest {
     private final Allele a01010102N = new Allele("http://immunogenomics.org/allele/1", "HLA02169", "HLA-A*01:01:01:02N", a);
     private final Allele a010102 = new Allele("http://immunogenomics.org/allele/2", "HLA01244", "HLA-A*01:01:02", a);
     private final Allele b0710 = new Allele("http://immunogenomics.org/allele/3", "HLA00142", "HLA-B*07:10", b);
-    private final Allele b0711 = new Allele("http://immunogenomics.org/allele/4", "HLA00143", "HLA-B*07:11", b);
+    private final Allele b0711G = new Allele("http://immunogenomics.org/allele/4", "HLA00143", "HLA-B*07:11G", b);
     private final Allele b0748 = new Allele("http://immunogenomics.org/allele/5", "HLA02514", "HLA-B*07:48", b);
     private final Allele b0749N = new Allele("http://immunogenomics.org/allele/6", "HLA02519", "HLA-B*07:49N", b);
     private final Allele b0750 = new Allele("http://immunogenomics.org/allele/7", "HLA02560", "HLA-B*07:50", b);
@@ -57,7 +57,7 @@ public final class AlleleComparatorsTest {
     private final Allele b0799 = new Allele("http://immunogenomics.org/allele/11", "HLA04250", "HLA-B*07:99", b);
     private final Allele b07100 = new Allele("http://immunogenomics.org/allele/12", "HLA04861", "HLA-B*07:100", b);
     private final List<Allele> unsorted = ImmutableList.of(a01010101, a01010102N, a010102,
-        b0710, b0711, b0748, b0748, b0749N, b0750, b0795, b079601, b079602, b0799, b07100);
+        b0710, b0711G, b0748, b0748, b0749N, b0750, b0795, b079601, b079602, b0799, b07100);
 
     @Test
     public void testByAccessionAscending() {
@@ -68,7 +68,7 @@ public final class AlleleComparatorsTest {
 
         assertEquals(a01010101, sorted.get(0));
         assertEquals(b0710, sorted.get(1));
-        assertEquals(b0711, sorted.get(2));
+        assertEquals(b0711G, sorted.get(2));
         assertEquals(a010102, sorted.get(3));
         assertEquals(a01010102N, sorted.get(4));
         assertEquals(b0748, sorted.get(5));
@@ -102,7 +102,7 @@ public final class AlleleComparatorsTest {
         assertEquals(b0748, sorted.get(9));
         assertEquals(a01010102N, sorted.get(10));
         assertEquals(a010102, sorted.get(11));
-        assertEquals(b0711, sorted.get(12));
+        assertEquals(b0711G, sorted.get(12));
         assertEquals(b0710, sorted.get(13));
         assertEquals(a01010101, sorted.get(14));
     }
@@ -118,7 +118,7 @@ public final class AlleleComparatorsTest {
         assertEquals(a01010102N, sorted.get(1));
         assertEquals(a010102, sorted.get(2));
         assertEquals(b0710, sorted.get(3));
-        assertEquals(b0711, sorted.get(4));
+        assertEquals(b0711G, sorted.get(4));
         assertEquals(b0748, sorted.get(5));
         assertEquals(b0748, sorted.get(6));
         assertEquals(b0749N, sorted.get(7));
@@ -147,7 +147,7 @@ public final class AlleleComparatorsTest {
         assertEquals(b0749N, sorted.get(7));
         assertEquals(b0748, sorted.get(8));
         assertEquals(b0748, sorted.get(9));
-        assertEquals(b0711, sorted.get(10));
+        assertEquals(b0711G, sorted.get(10));
         assertEquals(b0710, sorted.get(11));
         assertEquals(a010102, sorted.get(12));
         assertEquals(a01010102N, sorted.get(13));
