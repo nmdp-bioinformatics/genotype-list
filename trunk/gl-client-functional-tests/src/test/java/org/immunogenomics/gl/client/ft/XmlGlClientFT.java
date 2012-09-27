df150715c593cf2fe78f2f6f02381236f795b95d
mheuer@nmdp.org <mheuer@nmdp.org@a202cc79-c986-5676-df02-e9c59fcde34c>
2012-09-27 17:06:35 +0000

    adding xml client, functional tests fail

---
+++ trunk/gl-client-functional-tests/src/test/java/org/immunogenomics/gl/client/ft/XmlGlClientFT.java
@@ -0,0 +1,48 @@
+/*
+
+    gl-client-functional-tests  Functional tests for the client library for the
+    URI-based RESTful service for the gl project.
+    Copyright (c) 2012 National Marrow Donor Program (NMDP)
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.immunogenomics.gl.client.ft;
+
+import org.junit.BeforeClass;
+
+import org.immunogenomics.gl.client.GlClient;
+
+import org.immunogenomics.gl.client.xml.XmlGlClient;
+
+/**
+ * Functional test for XmlGlClient.
+ */
+public final class XmlGlClientFT  extends AbstractGlClientFT {
+    private static XmlGlClient xmlClient;
+
+    @BeforeClass
+    public static void staticSetUp() {
+        xmlClient = new XmlGlClient("http://localhost:10080/gl/");
+    }
+
+    @Override
+    protected GlClient createGlClient() {
+        return xmlClient;
+    }
+}
