14ea67e0893332f58554950a4965a44411306fa0
mgeorge@nmdp.org <mgeorge@nmdp.org@a202cc79-c986-5676-df02-e9c59fcde34c>
2012-10-18 15:21:15 +0000

    Denial-of-Service filter

---
+++ trunk/gl-web/src/main/java/org/immunogenomics/gl/web/dosfilter/DenialOfServiceConfigMBean.java
@@ -0,0 +1,38 @@
+package org.immunogenomics.gl.web.dosfilter;
+
+public interface DenialOfServiceConfigMBean {
+
+    String toString();
+
+    /** Return the number of hits that are not restricted. */
+    int getFreeHitCount();
+
+    void setFreeHitCount(int freeHitCount);
+
+    /** Return maximum number of hits per minute for anonymous access. */
+    int getAnonymousHitsPerMinute();
+
+    void setAnonymousHitsPerMinute(int anonymousHitsPerMinute);
+
+    long getCleanInterval();
+
+    void setCleanInterval(long cleanInterval);
+
+    void updateCleanIntervalInMinutes(String cleanIntervalMin) throws NumberFormatException;
+
+    void updateFreeHitCount(String hitCountStr);
+
+    void updateAnonymousHitsPerMinute(String hitCountStr);
+
+    /** Return the delay in milliseconds to wait before providing a response. */
+    long getThrottleDelay();
+
+    void setThrottleDelay(long throttleDelay);
+
+    void updateThrottleDelay(String msString);
+    
+    /** Returns the name of a request parameter that is required for authorized access. 
+     * If present, the client is considered authorized.  A separate SecurityFilter should actually test authentication and authorization. */
+    String getAuthorizationParamName();
+
+}
