e650eec1319bf45da0151d178e40fd596a310a54
Michael Heuer (NMDP) <mheuer@nmdp.org>
2013-03-25 18:57:07 +0000

    checkstyle fixes

--- gl-client/src/main/java/org/immunogenomics/gl/client/http/GlClientHttpException.java
+++ gl-client/src/main/java/org/immunogenomics/gl/client/http/GlClientHttpException.java
@@ -1,43 +1,90 @@
+/*
+
+    gl-client  Client library for the URI-based RESTful service for the gl project.
+    Copyright (c) 2012-2013 National Marrow Donor Program (NMDP)
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
 package org.immunogenomics.gl.client.http;
 
 /**
- * RuntimeException used to report HTTP failures.
- * @author mgeorge
+ * RuntimeException used to report HTTP failure.
  *
+ * @author mgeorge
  */
-public class GlClientHttpException extends RuntimeException {
+public final class GlClientHttpException extends RuntimeException {
+    private final int statusCode;
 
-    private static final long serialVersionUID = 1L;
-    
-    private int statusCode;
-    
-    public GlClientHttpException(int statusCode) {
+    /**
+     * Create a new gl client HTTP exception with the specified HTTP status code.
+     *
+     * @param statusCode HTTP status code
+     */
+    public GlClientHttpException(final int statusCode) {
         this.statusCode = statusCode;
     }
 
-    public GlClientHttpException(int statusCode, String message) {
+    /**
+     * Create a new gl client HTTP exception with the specified HTTP status code and message.
+     *
+     * @param statusCode HTTP status code
+     * @param message message
+     */
+    public GlClientHttpException(final int statusCode, final String message) {
         super(message);
         this.statusCode = statusCode;
     }
 
-    public GlClientHttpException(int statusCode, Throwable cause) {
+    /**
+     * Create a new gl client HTTP exception with the specified HTTP status code and cause.
+     *
+     * @param statusCode HTTP status code
+     * @param cause cause
+     */
+    public GlClientHttpException(final int statusCode, final Throwable cause) {
         super(cause);
         this.statusCode = statusCode;
     }
 
-    public GlClientHttpException(int statusCode, String message, Throwable cause) {
+    /**
+     * Create a new gl client HTTP exception with the specified HTTP status code, message, and cause.
+     *
+     * @param statusCode HTTP status code
+     * @param message message
+     * @param cause cause
+     */
+    public GlClientHttpException(final int statusCode, final String message, final Throwable cause) {
         super(message, cause);
         this.statusCode = statusCode;
     }
-    
+
     @Override
     public String toString() {
         return super.toString();
     }
 
-    /** HTTP Status Code */
+    /**
+     * Return the HTTP status code for this gl client HTTP exception.
+     *
+     * @return the HTTP status code for this gl client HTTP exception
+     */
     public int getStatusCode() {
         return statusCode;
     }
-
 }
--- gl-client/src/main/java/org/immunogenomics/gl/client/http/package-info.java
+++ gl-client/src/main/java/org/immunogenomics/gl/client/http/package-info.java
@@ -21,33 +21,8 @@
     > http://www.opensource.org/licenses/lgpl-license.php
 
 */
-package org.immunogenomics.gl.client.http;
-
-import java.io.InputStream;
 
 /**
- * Interface to decouple GlClient from HTTP communication. Mainly, to simplify JUnit testing of error handling.
+ * Http support for gl client implementations.
  */
-public interface HttpGetOrPost {
-
-    /**
-     * Return the content body as an InputStream.
-     *
-     * @param url location to get
-     * @param bearerToken optional OAuth 2.0 bearer token
-     * @return InputStream for content body which should be closed
-     * @throws GlClientHttpException when status code is not OK
-     */
-    InputStream get(String url, String bearerToken) throws GlClientHttpException;
-
-    /**
-     * Returns the "Location" header as a string.
-     *
-     * @param url location to get
-     * @param body "text/plain" content sent to the server
-     * @param bearerToken optional OAuth 2.0 bearer token
-     * @return the "Location" header
-     * @throws GlClientHttpException when status code is not OK
-     */
-    String post(String url, String body, String bearerToken) throws GlClientHttpException;
-}
+package org.immunogenomics.gl.client.http;
