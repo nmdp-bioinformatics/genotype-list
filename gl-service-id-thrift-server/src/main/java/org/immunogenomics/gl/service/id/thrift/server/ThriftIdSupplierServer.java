e7ce0fc0a67cf20730e0a6f96897fea16b7a69a0
Michael Heuer (NMDP) <mheuer@nmdp.org>
2013-05-02 15:50:27 +0000

    first checked in

---
+++ gl-service-id-thrift-server/src/main/java/org/immunogenomics/gl/service/id/thrift/server/ThriftIdSupplierServer.java
@@ -0,0 +1,86 @@
+/*
+
+    gl-service-id-thrift-server  Thrift distributed identifier supplier server.
+    Copyright (c) 2012-2013 National Marrow Donor Program (NMDP)
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.immunogenomics.gl.service.id.thrift.server;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import com.google.inject.Inject;
+
+import org.immunogenomics.gl.service.IdSupplier;
+
+import org.immunogenomics.gl.service.id.thrift.ThriftIdSupplier;
+
+/**
+ * Thrift identifier supplier server, adapts IdSupplier to ThriftIdSupplier.
+ */
+final class ThriftIdSupplierServer implements ThriftIdSupplier {
+    private final IdSupplier idSupplier;
+
+    @Inject
+    ThriftIdSupplierServer(final IdSupplier idSupplier) {
+        checkNotNull(idSupplier);
+        this.idSupplier = idSupplier;
+    }
+
+
+    @Override
+    public String createLocusId() {
+        return idSupplier.createLocusId();
+    }
+
+    @Override
+    public String createAlleleId() {
+        return idSupplier.createAlleleId();
+    }
+    
+    @Override
+    public String createAlleleListId() {
+        return idSupplier.createAlleleListId();
+    }
+
+    @Override
+    public String createHaplotypeId() {
+        return idSupplier.createHaplotypeId();
+    }
+
+    @Override
+    public String createGenotypeId() {
+        return idSupplier.createGenotypeId();
+    }
+
+    @Override
+    public String createGenotypeListId() {
+        return idSupplier.createGenotypeListId();
+    }
+
+    @Override
+    public String createMultilocusUnphasedGenotypeId() {
+        return idSupplier.createMultilocusUnphasedGenotypeId();
+    }
+
+    @Override
+    public void close() {
+        // empty
+    }
+}
---
+++ gl-service-id-thrift/src/test/java/org/immunogenomics/gl/service/id/thrift/ThriftIdSupplierModuleTest.java
@@ -0,0 +1,95 @@
+/*
+
+    gl-service-id-thrift  Distributed identifier supplier implementation based on Thrift.
+    Copyright (c) 2012-2013 National Marrow Donor Program (NMDP)
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.immunogenomics.gl.service.id.thrift;
+
+import static org.junit.Assert.assertNotNull;
+
+import com.facebook.swift.codec.guice.ThriftCodecModule;
+
+import com.facebook.swift.service.ThriftClient;
+import com.facebook.swift.service.guice.ThriftClientModule;
+
+import com.google.common.collect.ImmutableMap;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+import com.google.inject.Stage;
+import com.google.inject.TypeLiteral;
+
+import io.airlift.configuration.ConfigurationFactory;
+import io.airlift.configuration.ConfigurationModule;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import org.immunogenomics.gl.service.IdSupplier;
+
+/**
+ * Unit test for ThriftIdSupplierModule.
+ */
+public final class ThriftIdSupplierModuleTest {
+    private ThriftIdSupplierModule thriftIdSupplierModule;
+
+    @Before
+    public void setUp() {
+        thriftIdSupplierModule = new ThriftIdSupplierModule();
+    }
+
+    @Test
+    public void testConstructor() {
+        assertNotNull(thriftIdSupplierModule);
+    }
+
+    @Test
+    public void testThriftIdSupplierModule() {
+        Injector injector = Guice.createInjector(Stage.PRODUCTION,
+                                                 new TestModule(),
+                                                 new ConfigurationModule(new ConfigurationFactory(ImmutableMap.<String, String>of())),
+                                                 new ThriftCodecModule(),
+                                                 new ThriftClientModule(),
+                                                 thriftIdSupplierModule);
+        assertNotNull(injector);
+
+        // client
+        ThriftClient<ThriftIdSupplier> thriftClient = injector.getInstance(Key.get(new TypeLiteral<ThriftClient<ThriftIdSupplier>>() {}));
+        assertNotNull(thriftClient);
+
+        // client --> IdSupplier
+        IdSupplier idSupplier = injector.getInstance(IdSupplier.class);
+        assertNotNull(idSupplier);
+    }
+
+    /**
+     * Test module.
+     */
+    private class TestModule extends AbstractModule {
+
+        @Override
+        protected void configure() {
+            // empty
+        }
+    }
+}
