5dd38a5f48218bde64995c00041e6bf2df4bd0c5
Michael Heuer (NMDP) <mheuer@nmdp.org>
2013-08-01 17:24:29 +0000

    Issue 119 ; adding gl-service-id-jdbc module with mysql implementation

---
+++ gl-service-id-jdbc/src/main/sql/gl-service-id-jdbc-mysql-create.sql
@@ -0,0 +1,12 @@
+create table if not exists `sequence` (
+ `sequence_name` varchar(32) unique not null,
+ `sequence_value` unsigned bigint not null
+) engine=innodb;
+
+insert into `sequence`(`sequence_name`, `sequence_value`) values (`locus`, 0);
+insert into `sequence`(`sequence_name`, `sequence_value`) values (`allele`, 0);
+insert into `sequence`(`sequence_name`, `sequence_value`) values (`allele-list`, 0);
+insert into `sequence`(`sequence_name`, `sequence_value`) values (`haplotype`, 0);
+insert into `sequence`(`sequence_name`, `sequence_value`) values (`genotype`, 0);
+insert into `sequence`(`sequence_name`, `sequence_value`) values (`genotype-list`, 0);
+insert into `sequence`(`sequence_name`, `sequence_value`) values (`multilocus-unphased-genotype`, 0);
