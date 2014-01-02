3ae7d5d1baa8db4d1462b215af43d2a72e4b62b0
mheuer@nmdp.org <mheuer@nmdp.org@a202cc79-c986-5676-df02-e9c59fcde34c>
2014-01-02 20:56:10 +0000

    Issue 159 ; update copyright year to include 2014

--- trunk/gl-service-jdbc/src/test/java/org/immunogenomics/gl/service/jdbc/JdbcUtilsTest.java
+++ trunk/gl-service-jdbc/src/test/java/org/immunogenomics/gl/service/jdbc/JdbcUtilsTest.java
@@ -1,7 +1,7 @@
 /*
 
     gl-service-jdbc  Implementation of persistent cache for gl-service using JDBC.
-    Copyright (c) 2012-2013 National Marrow Donor Program (NMDP)
+    Copyright (c) 2012-2014 National Marrow Donor Program (NMDP)
 
     This library is free software; you can redistribute it and/or modify it
     under the terms of the GNU Lesser General Public License as published
