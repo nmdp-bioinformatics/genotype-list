571661fc20d539b11a6a6b00ee93d1632eaae134
mheuer@nmdp.org <mheuer@nmdp.org@a202cc79-c986-5676-df02-e9c59fcde34c>
2015-01-21 23:18:20 +0000

    Issue 61 ; adding ambiguity service client

---
+++ trunk/gl-ambiguity-client/src/main/java/org/immunogenomics/gl/ambiguity/client/AmbiguityServiceModule.java
@@ -0,0 +1,51 @@
+/*
+
+    gl-ambiguity-client  Client library for RESTful genotype list ambiguity service.
+    Copyright (c) 2012-2014 National Marrow Donor Program (NMDP)
+
+    This library is free software; you can redistribute it and/or modify it
+    under the terms of the GNU Lesser General Public License as published
+    by the Free Software Foundation; either version 3 of the License, or (at
+    your option) any later version.
+
+    This library is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; with out even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public
+    License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with this library;  if not, write to the Free Software Foundation,
+    Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA.
+
+    > http://www.fsf.org/licensing/licenses/lgpl.html
+    > http://www.opensource.org/licenses/lgpl-license.php
+
+*/
+package org.immunogenomics.gl.ambiguity.client;
+
+import javax.annotation.concurrent.Immutable;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+import retrofit.RestAdapter;
+
+/**
+ * Ambiguity service [client] module.
+ */
+@Immutable
+public final class AmbiguityServiceModule extends AbstractModule {
+
+    @Override
+    protected void configure() {
+        // empty
+    }
+
+    @Provides @Singleton
+    static AmbiguityService createAmbiguityService(@EndpointUrl final String endpointUrl) {
+        return new RestAdapter.Builder()
+            .setEndpoint(endpointUrl)
+            .build().create(AmbiguityService.class);
+    }
+}
